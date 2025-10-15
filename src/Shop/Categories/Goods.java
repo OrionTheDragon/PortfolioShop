f6d2a2d6c97b5eb84463ea13dbd249ce0b5848a9
XxDragonFoxX <golub.kd@mail.ru>
2025-10-15 19:30:41 +0300

    Auto-commit at 15.10.2025 19:30:41,17

--- src/Shop/Categories/Goods.java
+++ src/Shop/Categories/Goods.java
@@ -624,7 +624,7 @@ public class Goods {
     }
 
     public void addSQL() {
-        String sql = "INSERT INTO Goods (SKU, productName, manufacturer, country, )";
+        String sql = "INSERT INTO Goods (SKU, productName, manufacturer, country, categories, subCategories, type, price, quantity) ";
     }
 
     @Override
